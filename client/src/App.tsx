4edd67debf6f8f8a90f6fc2515f67bd79c621ef5
dahl898 <wdmakota@gmail.com>
2023-04-16 23:51:03 +0200

    Change url of fetch function in App component to '/login'

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   const [loading, setLoading] = useState<boolean>(true)
 
   const fetchData = async () => {
-    const response = await fetch(`http://localhost:${process.env.PORT}/login`,  {
+    const response = await fetch('/login',  {
       method: 'GET',
       mode: 'cors',
       credentials: 'include'
