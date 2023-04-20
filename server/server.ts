233b29f3c20d5da6c56a7bc3106371e784bfc108
dahl898 <wdmakota@gmail.com>
2023-04-20 16:38:29 +0200

    Change origin to localhost 5000

--- server/server.ts
+++ server/server.ts
@@ -17,7 +17,7 @@ const Account = connection.models.Account;
 const User = connection.models.User;
 
 app.use(cors({
-  origin: 'http://localhost:3000', //change before build to 'http://localhost:5000'
+  origin: 'http://localhost:5000', //change before build to 'http://localhost:5000'
   methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE'],
   credentials: true
 }));
